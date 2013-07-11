bf9062f917527ef3b12d437688e294b93bcea362
Mykola Prodanov <prodk@rambler.ru>
2013-07-11 16:19:20 +0200

    Modified web.js to use text from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var inputfile = "index.html";
+var buf = fs.readFileSync(inputfile);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
